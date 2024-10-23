95cd92e5a53336f462977a7a6f16ead4a50d1d46
Alex Lee <alexjlee.dev@gmail.com>
2024-10-23 23:24:06 +0100

    feat: add share button in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,31 @@
 import { NavLink } from 'react-router-dom';
+import { SquareArrowOutUpRight } from 'lucide-react';
+
+import { Button } from '@/components/ui/button';
 
 const Header = () => {
+  const copyUrl = () => {
+    const homeUrl = `${window.location.origin}`;
+    navigator.clipboard
+      .writeText(homeUrl)
+      .then(() => {
+        console.log('Copied');
+      })
+      .catch((err) => {
+        console.log('Failed to copy', err);
+      });
+  };
+
   return (
-    <header className='w-full bg-zinc-950'>
+    <header className='dark w-full bg-zinc-950'>
       <div className='flex justify-between items-center max-w-4xl mx-auto px-4 py-4 text-zinc-50'>
         <NavLink to='/'>
           <span className='italic font-semibold'>_of this</span>
         </NavLink>
-        {/* Share Button */}
+        <Button onClick={copyUrl} variant='outline' size='sm'>
+          <SquareArrowOutUpRight />
+          <span>공유하기</span>
+        </Button>
       </div>
     </header>
   );
