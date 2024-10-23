84e67ef74211965c716b8e5cd3c08b1147f8884f
Alex Lee <alexjlee.dev@gmail.com>
2024-10-23 23:06:33 +0100

    feat: add basic styling to header and footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,9 @@
 const Footer = () => {
   return (
-    <footer>
-      <p>© 2024 alexjleee. All rights reserved.</p>
+    <footer className='w-full border-t border-zinc-200'>
+      <p className='max-w-4xl mx-auto px-4 py-4 text-center text-zinc-500'>
+        © 2024 alexjleee. All rights reserved.
+      </p>
     </footer>
   );
 };
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,13 @@ import { NavLink } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <header>
-      <NavLink to='/'>
-        <span>Of this</span>
-      </NavLink>
+    <header className='w-full bg-zinc-950'>
+      <div className='flex justify-between items-center max-w-4xl mx-auto px-4 py-4 text-zinc-50'>
+        <NavLink to='/'>
+          <span className='italic font-semibold'>_of this</span>
+        </NavLink>
+        {/* Share Button */}
+      </div>
     </header>
   );
 };
