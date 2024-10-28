b910c3ab481228c2a01ef017bb1a05e3155333cd
Alex Lee <alexjlee.dev@gmail.com>
2024-10-28 10:33:49 +0000

    feat: make main content area to fill the remaining space

--- src/App.tsx
+++ src/App.tsx
@@ -11,14 +11,16 @@ import MonthCustom from '@/pages/MonthCustom';
 import MonthSummary from '@/pages/MonthSummary';
 
 const AppLayout: FC<PropsWithChildren> = ({ children }) => (
-  <>
+  <div className='flex flex-col w-full h-screen min-h-fit'>
     <Header />
-    <main className='w-full bg-zinc-50 text-zinc-950'>
-      <div className='max-w-4xl mx-auto px-4 py-8'>{children}</div>
+    <main className='flex-1 w-full h-full min-h-fit bg-zinc-50 text-zinc-950'>
+      <div className='max-w-4xl h-full min-h-fit mx-auto px-4 py-8'>
+        {children}
+      </div>
     </main>
     <Footer />
     <Toaster />
-  </>
+  </div>
 );
 
 function App() {
